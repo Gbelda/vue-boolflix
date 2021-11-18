c0157e52fa9cd39450d476940ed38d265d3756f2
gbelda <gbelda56@gmail.com>
2021-11-18 15:22:11 +0100

    Apply getflagemoji function

--- src/components/Movie.vue
+++ src/components/Movie.vue
@@ -2,18 +2,32 @@
   <div class="col-3 movie">
     <h1>{{ title }}</h1>
     <h4>{{ original_title }}</h4>
-    <h4>{{ language }}</h4>
+    <h4>{{ getFlagEmoji(language) }}</h4>
     <h4>vote average : {{ vote_avg }}</h4>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      code: this.language,
+    };
+  },
   props: {
     title: String,
     original_title: String,
     language: String,
     vote_avg: Number,
+  },
+  methods: {
+    getFlagEmoji(countryCode) {
+      const codePoints = countryCode
+        .toUpperCase()
+        .split("")
+        .map((char) => 127397 + char.charCodeAt());
+      return String.fromCodePoint(...codePoints);
+    },
   },
 };
 </script>
